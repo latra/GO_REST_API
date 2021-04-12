fb31a378f7b2a79428735eddc8eddd192f77b923
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-04-12 21:24:55 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module WebListener
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/gorilla/mux v1.8.0
+)
---
+++ weblistener.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gorilla/mux"
+)
+
+type Message struct {
+	Direction string `json:"IP"`
+	Port      string `json:"Port"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pero tu que tramas, morena")
+	fmt.Println("Endpoint Hit: homePage")
+}
+func sendToBots(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Message sended via bots")
+	discord, err := discordgo.New("Bot " + "")
+	fmt.Println(err)
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var message Message
+	json.Unmarshal(reqBody, &message)
+	discord.ChannelMessageSend("404371919618703371", fmt.Sprintf("Servidord de terraria abierto en la IP %s\nPuerto: %s.", message.Direction, message.Port))
+}
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/send", sendToBots).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func main() {
+	handleRequests()
+}
